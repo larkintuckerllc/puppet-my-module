30986fbbd883fe4665751ae9d8b24bc6e72774ba
John Tucker <jtucker@spotify.com>
2024-10-31 09:38:58 -0400

    module

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include my_module
+class my_module {
+  notify { "hello world": }
+}
